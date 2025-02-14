7e5adf16ad257c9e54eb2171b4c804d358d5b29d
Amr Abd El-Hamid Mahmoud <amr.said82@hotmail.com>
2025-02-14 16:43:20 +0400

    Init commit

---
+++ CurrencyConverter.DBChanges/dbo/Tables/CurrencyLogs.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CurrencyLogs] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [IsActive]   BIT           NOT NULL,
+    [IsDeleted]  BIT           NOT NULL,
+    [CreatedAt]  DATETIME2 (7) NOT NULL,
+    [ModifiedAt] DATETIME2 (7) NULL,
+    CONSTRAINT [PK_CurrencyLogs] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ CurrencyConverter.DBChanges/dbo/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NULL,
+    [Email]      NVARCHAR (MAX) NULL,
+    [Password]   NVARCHAR (MAX) NULL,
+    [Roles]      NVARCHAR (MAX) NULL,
+    [IsActive]   BIT            NOT NULL,
+    [IsDeleted]  BIT            NOT NULL,
+    [CreatedAt]  DATETIME2 (7)  NOT NULL,
+    [ModifiedAt] DATETIME2 (7)  NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
